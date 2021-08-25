9a52f2825ee23ac5bfff2e0751eb7a2616ede392
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-25 22:58:45 +0530

    [Nikhil] - added frequency of words

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -1,28 +1,53 @@
+import java.util.ArrayList;
+
 public class MyHashMap<K,V> {
-    MyLinkedList<K> myLinked;
+
+    private final int numBuckets;
+    ArrayList<MyLinkedList<K>> myBucketArray;
 
     public MyHashMap() {
-        this.myLinked = new MyLinkedList<>();
-    }
-
-    public V get( K key) {
-        MyMapNode<K, V> myMapNode = (MyMapNode<K,V>) this.myLinked.search(key);
-        return (myMapNode == null) ? null : myMapNode.getValue();
-    }
-
-    public void add (K key, V value) {
-        MyMapNode<K, V> myMapNode = (MyMapNode<K,V>) this.myLinked.search(key);
-        if (myMapNode == null) {
-            myMapNode = new MyMapNode<>(key,value);
-            this.myLinked.append(myMapNode);
+        this.numBuckets = 10;
+        this.myBucketArray = new ArrayList<>(numBuckets);
+        for(int i=0;i<numBuckets;i++) {
+            this.myBucketArray.add(null);
         }
-        else{
+
+    }
+
+    private int getBucketIndex(K key) {
+        int hashcode = Math.abs(key.hashCode());
+        int index = hashcode%numBuckets;
+        return index;
+    }
+
+    public V get(K key) {
+        int index = this.getBucketIndex(key);
+        MyLinkedList myLinked = this.myBucketArray.get(index);
+        if (myLinked == null) return null;
+        MyMapNode<K, V> myMapNode = (MyMapNode<K,V>) myLinked.search(key);
+        return (myMapNode == null)? null:myMapNode.getValue();
+    }
+
+    public void add(K key, V value) {
+        int index = this.getBucketIndex(key);
+        MyLinkedList myLinked = this.myBucketArray.get(index);
+        if (myLinked == null) {
+            myLinked = new MyLinkedList<>();
+            this.myBucketArray.set(index, myLinked);
+        }
+        MyMapNode<K, V> myMapNode = (MyMapNode<K,V>) myLinked.search(key);
+        if (myMapNode==null) {
+            myMapNode = new MyMapNode<>(key,value);
+            myLinked.append(myMapNode);
+        }
+        else {
             myMapNode.setValue(value);
         }
+
     }
 
     @Override
     public String toString() {
-        return "MyHashMapNodes{" + myLinked + "}" ;
+        return "MyLinkedHashMap List{"+ myBucketArray+"}";
     }
 }
--- src/MyHashMapRunner.java
+++ src/MyHashMapRunner.java
@@ -5,15 +5,18 @@ public class MyHashMapRunner {
     }
 
     public void givenString(){
-        String sentence = "To be or not to be";
-        MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
-        String[] words = sentence.toLowerCase().split(" ");
-        for(String word : words) {
-            Integer value = myHashMap.get(word);
+        String paragraph = "Paranoids are not paranoid because they are paranoid "
+                + "but because they keep putting themselves "
+                + "deliberately into paranoid avoidable situations";
+
+        MyHashMap<String, Integer> myLinkedHashMap = new MyHashMap<>();
+        String[] words = paragraph.toLowerCase().split(" ");
+        for( String word : words) {
+            Integer value = myLinkedHashMap.get(word);
             if (value == null) value = 1;
             else value ++;
-            myHashMap.add(word, value);
+            myLinkedHashMap.add(word, value);
         }
-        System.out.println(myHashMap);
+        System.out.println(myLinkedHashMap);
     }
 }
